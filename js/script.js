8488ecb2a74b796b85200a2d82d1b6526c9389e9
XzeelKetche <raihanf432@gmail.com>
2025-03-19 20:51:56 +0700

    `Update index.html and script.js to use Lenis 1.2.3 and Tailwind CSS 4`

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-const lenis = new Lenis({
-    duration: 1.2,
-    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-    autoRaf: true,
-    anchors: true,
-});
-
 /**
  __  __              _ _  __    _       _          
  \ \/ / _______  ___| | |/ /___| |_ ___| |__   ___ 
